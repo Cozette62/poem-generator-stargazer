cf8db4c68134337fca47fda21dacf89edf7b9964
Cozette Gagne <156377760+Cozette62@users.noreply.github.com>
2024-06-22 16:13:15 -0400

    Added API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let poemElement = document.querySelector("#poem");
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "La vie est belle en rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "1730e2a9b8853t39f40ob3d474fe90da";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic poem expert who loves to write short 4-line poems in HTML. Your mission is to generate a poem by following the user instructions below.";
+  apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
