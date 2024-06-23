97b869b68c8d25cb8d7693e92051102346ae9840
Cozette Gagne <156377760+Cozette62@users.noreply.github.com>
2024-06-23 14:23:13 -0400

    Made a few adjustments to the prompt and context of the AI API

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let apiKey = "1730e2a9b8853t39f40ob3d474fe90da";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let context =
-    "You are a romantic poem expert who loves to write short 4-line poems in HTML. Your mission is to generate a poem by following the user instructions below.";
+    "You are a romantic poem expert who loves to write short 4-line poems in basic HTML and separate each line with a <br />. Do not include a title in the poem. Your mission is to generate a poem by following the user instructions below. Sign the poem with `SheCodes AI` inside a <strong> element at the end of the poem";
   apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiURL).then(displayPoem);
